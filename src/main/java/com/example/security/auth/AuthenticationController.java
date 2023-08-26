7e4e79b829f60e9122953ea627666ef4c9ddec9c
janithamadubashana <madubashanajanitha@gmail.com>
2023-08-26 20:13:51 +0530

    Add SpringDoc /swagger 3 allow permission for swagger requests by SecurityConfiguration class

--- src/main/java/com/example/security/auth/AuthenticationController.java
+++ src/main/java/com/example/security/auth/AuthenticationController.java
@@ -1,28 +1,31 @@
 package com.example.security.auth;
-
+//
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.ApiResponse;
+//import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin
 public class AuthenticationController {
+
     private final AuthenticationService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody RegisterRequest request
-    ) {
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(service.register(request));
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(
-            @RequestBody AuthenticationRequest request
-    ) {
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(service.authenticate(request));
     }
 }
--- src/main/java/com/example/security/config/SecurityConfiguration.java
+++ src/main/java/com/example/security/config/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
+                .requestMatchers("/v3/**", "/swagger-ui/**","/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
