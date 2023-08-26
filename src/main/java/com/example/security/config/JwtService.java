df899f2164788cd817abb618b00b6db52b35ddb0
janithamadubashana <madubashanajanitha@gmail.com>
2023-08-26 11:15:55 +0530

    add Jwt dependencies to pom.xml

--- src/main/java/com/example/security/config/JwtService.java
+++ src/main/java/com/example/security/config/JwtService.java
@@ -1,4 +1,11 @@
 package com.example.security.config;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class JwtService {
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
 }
