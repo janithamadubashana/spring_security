1f4ef42c43051a731a05657f5522b0f148ba71bd
janithamadubashana <madubashanajanitha@gmail.com>
2023-08-26 17:49:22 +0530

    AuthenticationService classes was created

--- src/main/java/com/example/security/auth/AuthenticationController.java
+++ src/main/java/com/example/security/auth/AuthenticationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
-
+    private final AuthenticationService service;
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
---
+++ src/main/java/com/example/security/auth/AuthenticationService.java
@@ -0,0 +1,9 @@
+package com.example.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+}
